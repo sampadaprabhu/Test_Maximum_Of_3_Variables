694e36ecfd4fc55216c3dd57a8c52ff5135c35b4
shindepriti <pritishinde34@gmail.com>
2020-02-28 18:21:21 +0530

    REFACTORED: Create Generic Class To Take 3 Variables Of Generic Type

--- src/main/java/com/bridgelabz/MaximumNumber.java
+++ src/main/java/com/bridgelabz/MaximumNumber.java
@@ -1,6 +1,18 @@
 package com.bridgelabz;
 
-public class MaximumNumber {
+public class MaximumNumber <E extends Comparable<E>>{
+    E x;
+    E y;
+    E z;
+
+    public MaximumNumber(E x, E y, E z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public E checkMaximum(){
+        return getMaximum(x,y,z);
+    }
     public static <E extends Comparable<E>> E getMaximum(E x, E y, E z) {
         E maximum = x;
         if(y.compareTo(maximum)>0){
--- src/test/java/com/bridgelabz/maximumNumber.java
+++ src/test/java/com/bridgelabz/maximumNumber.java
@@ -6,63 +6,66 @@ import org.junit.Test;
 
 public class maximumNumber {
 
-    MaximumNumber maxNumber;
-    @Before
-    public void setUp() throws Exception {
-        maxNumber=new MaximumNumber();
-    }
-    //MaximumNumber maxNumber=new MaximumNumber();
     @Test
     public void testInteger1() {
-        int max=maxNumber.getMaximum(5,4,3);
+        MaximumNumber maxNumber=new MaximumNumber(5,4,3);
+        Comparable max = maxNumber.checkMaximum();
         Assert.assertEquals(5,max);
     }
 
     @Test
     public void testInteger2() {
-        int max=maxNumber.getMaximum(3,5,4);
+        MaximumNumber maxNumber=new MaximumNumber(3,5,4);
+        Comparable max=maxNumber.checkMaximum();
         Assert.assertEquals(5,max);
     }
 
     @Test
     public void testInteger3() {
-        int max=maxNumber.getMaximum(4,3,5);
+        MaximumNumber maxNumber=new MaximumNumber(4,3,5);
+        Comparable max=maxNumber.checkMaximum();
         Assert.assertEquals(5,max);
     }
 
     @Test
     public void testFloat1() {
-        float max=maxNumber.getMaximum(5.7f, 3.2f,4.3f);
-        Assert.assertEquals(5.7,max,1);
+        MaximumNumber maxNumber=new MaximumNumber(5.7f,3.2f,4.3f);
+        Comparable max=maxNumber.checkMaximum();
+        Assert.assertEquals(5.7, (Float) max,1);
     }
 
     @Test
     public void testFloat2() {
-        float max=maxNumber.getMaximum(4.3f, 5.7f, 3.2f);
-        Assert.assertEquals(5.7,max,1);
+        MaximumNumber maxNumber=new MaximumNumber(4.3f, 5.7f, 3.2f);
+        Comparable max=maxNumber.checkMaximum();
+        Assert.assertEquals(5.7, (Float) max,1);
     }
 
     @Test
     public void testFloat3() {
-        float max=maxNumber.getMaximum(3.2f, 4.3f, 5.7f);
-        Assert.assertEquals(5.7,max,1);
+        MaximumNumber maxNumber=new MaximumNumber(3.2f, 4.3f, 5.7f);
+        Comparable max=maxNumber.checkMaximum();
+        Assert.assertEquals(5.7, (Float) max,1);
     }
 
     @Test
     public void testString1() {
-        String max=maxNumber.getMaximum("Peach","Banana","Apple");
+        MaximumNumber maxNumber=new MaximumNumber("Peach","Banana","Apple");
+        Comparable max=maxNumber.checkMaximum();
         Assert.assertEquals("Largest Fruit Is : ", "Peach",max);
     }
 
     @Test
     public void testString2() {
-        String max=maxNumber.getMaximum("Apple","Peach","Banana");
+        MaximumNumber maxNumber=new MaximumNumber("Apple","Peach","Banana");
+        Comparable max=maxNumber.checkMaximum();
         Assert.assertEquals("Largest Friut Is : ","Peach",max);
     }
 
     @Test
     public void testString3() {
-        String max=maxNumber.getMaximum("Banana","Apple","Peach");
+        MaximumNumber maxNumber=new MaximumNumber("Banana","Apple","Peach");
+        Comparable max=maxNumber.checkMaximum();
         Assert.assertEquals("Largest Fruit Is : ","Peach",max);
     }
 }
