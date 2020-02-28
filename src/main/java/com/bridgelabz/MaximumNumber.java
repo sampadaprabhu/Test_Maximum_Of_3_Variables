56816f13c04bddb16f0287997a34dd672691771f
shindepriti <pritishinde34@gmail.com>
2020-02-28 17:06:21 +0530

    REFACTORED: All The Three To One Generic Method To Find Maximum

--- src/main/java/com/bridgelabz/MaximumNumber.java
+++ src/main/java/com/bridgelabz/MaximumNumber.java
@@ -1,34 +1,13 @@
 package com.bridgelabz;
 
 public class MaximumNumber {
-    public int getMaxIntegerNumber(int x, int y, int z) {
-        int maximum = x;
-        if(Integer.compare(y,maximum)>0){
+    public static <E extends Comparable<E>> E getMaximum(E x, E y, E z) {
+        E maximum = x;
+        if(y.compareTo(maximum)>0){
             maximum=y;
         }
-        if (Integer.compare(z,maximum)>0){
+        if (z.compareTo(maximum)>0){
             maximum=z;
-        }
-        return maximum;
-    }
-
-    public float getMaxFloatNumber(float x, float y, float z) {
-        float maximum = x;
-        if(Float.compare(y , maximum) > 0){
-            maximum= y;
-        }
-        if(Float.compare(z,maximum)>0){
-            maximum=z;
-        }
-        return maximum;
-    }
-    public String getMaxString(String fruit1, String fruit2, String fruit3) {
-        String maximum = fruit1;
-        if(fruit2.compareTo(maximum)>0){
-            maximum=fruit2;
-        }
-        if (fruit3.compareTo(maximum)>0){
-            maximum=fruit3;
         }
         return maximum;
     }
--- src/test/java/com/bridgelabz/maximumNumber.java
+++ src/test/java/com/bridgelabz/maximumNumber.java
@@ -14,56 +14,55 @@ public class maximumNumber {
     //MaximumNumber maxNumber=new MaximumNumber();
     @Test
     public void testInteger1() {
-        int max=maxNumber.getMaxIntegerNumber(5,4,3);
+        int max=maxNumber.getMaximum(5,4,3);
         Assert.assertEquals(5,max);
     }
 
     @Test
     public void testInteger2() {
-        int max=maxNumber.getMaxIntegerNumber(3,5,4);
+        int max=maxNumber.getMaximum(3,5,4);
         Assert.assertEquals(5,max);
     }
 
     @Test
     public void testInteger3() {
-        int max=maxNumber.getMaxIntegerNumber(4,3,5);
+        int max=maxNumber.getMaximum(4,3,5);
         Assert.assertEquals(5,max);
     }
 
     @Test
     public void testFloat1() {
-        float max=maxNumber.getMaxFloatNumber(5.7f, 3.2f,4.3f);
+        float max=maxNumber.getMaximum(5.7f, 3.2f,4.3f);
         Assert.assertEquals(5.7,max,1);
     }
 
     @Test
     public void testFloat2() {
-        float max=maxNumber.getMaxFloatNumber(4.3f, 5.7f, 3.2f);
+        float max=maxNumber.getMaximum(4.3f, 5.7f, 3.2f);
         Assert.assertEquals(5.7,max,1);
     }
 
     @Test
     public void testFloat3() {
-        float max=maxNumber.getMaxFloatNumber(3.2f, 4.3f, 5.7f);
+        float max=maxNumber.getMaximum(3.2f, 4.3f, 5.7f);
         Assert.assertEquals(5.7,max,1);
     }
 
     @Test
     public void testString1() {
-        String max=maxNumber.getMaxString("Peach","Banana","Apple");
+        String max=maxNumber.getMaximum("Peach","Banana","Apple");
         Assert.assertEquals("Largest Fruit Is : ", "Peach",max);
     }
 
     @Test
     public void testString2() {
-        String max=maxNumber.getMaxString("Apple","Peach","Banana");
+        String max=maxNumber.getMaximum("Apple","Peach","Banana");
         Assert.assertEquals("Largest Friut Is : ","Peach",max);
     }
 
     @Test
     public void testString3() {
-        String max=maxNumber.getMaxString("Banana","Apple","Peach");
+        String max=maxNumber.getMaximum("Banana","Apple","Peach");
         Assert.assertEquals("Largest Fruit Is : ","Peach",max);
     }
-
 }
