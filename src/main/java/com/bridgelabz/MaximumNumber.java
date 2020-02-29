24cb0f54d7814d794039e95fe33e8ccb22f039ea
shindepriti <pritishinde34@gmail.com>
2020-02-29 09:55:00 +0530

    REFACTORED : Function To Accept More Than Three Arguments And Find Maximum

--- src/main/java/com/bridgelabz/MaximumNumber.java
+++ src/main/java/com/bridgelabz/MaximumNumber.java
@@ -1,5 +1,8 @@
 package com.bridgelabz;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MaximumNumber <E extends Comparable<E>>{
     E x;
     E y;
@@ -13,13 +16,19 @@ public class MaximumNumber <E extends Comparable<E>>{
     public E checkMaximum(){
         return getMaximum(x,y,z);
     }
-    public static <E extends Comparable<E>> E getMaximum(E x, E y, E z) {
+    public static <E extends Comparable<E>> E getMaximum(E x, E y, E z, E... arrayValues) {
         E maximum = x;
         if(y.compareTo(maximum)>0){
             maximum=y;
         }
         if (z.compareTo(maximum)>0){
             maximum=z;
+        }
+        if(arrayValues.length !=0){
+            Arrays.sort(arrayValues, Collections.reverseOrder());
+            if (maximum.compareTo(arrayValues[0])<0){
+                maximum=arrayValues[0];
+            }
         }
         return maximum;
     }
